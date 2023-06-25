309bbe4dd9796a161fded198b20982d1aa3a41e5
Osiris8 <miganosiris8@gmaaail.com>
2023-06-25 09:13:49 +0200

    add top and left position of bubble with Math.random

--- js/script.js
+++ js/script.js
@@ -7,3 +7,8 @@ console.log(size);
 
 bubble.style.height = size;
 bubble.style.width = size;
+
+bubble.style.top = Math.random() * 100 + 10 + "%";
+bubble.style.left = Math.random() * 100 + "%";
+
+bubble.style.setProperty("--left", Math.random() * 100 + "%");
