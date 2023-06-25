0522f8e9cc49c41e0d2bbf912125c767b43f4a1b
Osiris8 <miganosiris8@gmaaail.com>
2023-06-25 08:56:08 +0200

    generate height and width of bubble with math.random

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 const bubble = document.createElement("span");
 bubble.classList.add("bubble");
 document.body.appendChild(bubble);
+
+const size = Math.random() * 200 + 100 + "px";
+console.log(size);
+
+bubble.style.height = size;
+bubble.style.width = size;
